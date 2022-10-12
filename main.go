4dfd964ef075da6f32fcb08eb9e410c1007315c8
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2022-10-12 20:22:26 +0300

    Created http server

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/cheatsnake/rest-api-vanilla-go/handlers"
+	"github.com/cheatsnake/rest-api-vanilla-go/models/taskstore"
+)
+
+var Store *taskstore.TaskStore
+
+func init() {
+	Store = taskstore.New()
+}
+
+func main() {
+	http.HandleFunc("/task/", handlers.TaskHandler)
+	http.HandleFunc("/tasks/", handlers.TasksHandler)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
